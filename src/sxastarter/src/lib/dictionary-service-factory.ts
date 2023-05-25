3b9fae41c5ab1aede48fa8c31780251cf86aa29c
Radif <radif@code.id>
2023-05-25 21:28:03 +0700

    adddd

--- src/sxastarter/src/lib/dictionary-service-factory.ts
+++ src/sxastarter/src/lib/dictionary-service-factory.ts
@@ -25,8 +25,8 @@ export class DictionaryServiceFactory {
             The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
             app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
             otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
-            rootItemId: '{GUID}'
           */
+          rootItemId: '{0FDBAE10-632C-47A2-9B23-A343C08BB8CC}'
         })
       : new RestDictionaryService({
           apiHost: config.sitecoreApiHost,
